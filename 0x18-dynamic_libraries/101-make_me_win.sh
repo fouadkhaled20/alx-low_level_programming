12746121e02d197cba16e1c80d970033387676ee
fouad khaled <fouad khaled@example.com>
2024-01-29 07:14:55 -0800

    a

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/fouadkhaled20/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+wget -P /tmp https://github.com/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/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
